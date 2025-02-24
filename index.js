90aa7ba6340010465404fb548067352a15c76ffb
mrice2003 <a00837460@tec.mx>
2025-02-24 10:59:13 -0600

    Added changes to tests branch

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-function calculate() {
-    const num1 = document.getElementById("num1").value;
-    const num2 = document.getElementById("num2").value;
+function calculate(operator) {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
     
     let result;
-    result = num1 + num2;
+    if (operator === '+') {
+        result = num1 + num2;
+    } else if (operator === '-') {
+        result = num1 - num2;
+    }
     
     document.getElementById("result").innerText = "Result: " + result.toFixed(2);
-  }
-  
-  function resetFields() {
+}
+
+function resetFields() {
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
-    document.getElementById("operator").value = "+";
     document.getElementById("result").innerText = "Result: ";
-  }
+}
