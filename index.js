6f428f981b575be8cc453432caaa89a12901f788
Michael Rice <156035751+mrice2003@users.noreply.github.com>
2025-03-06 08:49:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-function calculate(operator) {
-    const num1 = parseFloat(document.getElementById("num1").value);
-    const num2 = parseFloat(document.getElementById("num2").value);
+function calculate() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
     
     let result;
-    if (operator === '+') {
-        result = num1 + num2;
-    } else if (operator === '-') {
-        result = num1 - num2;
-    }
+    result = num1 + num2;
     
     document.getElementById("result").innerText = "Result: " + result.toFixed(2);
-}
-
-function resetFields() {
+  }
+  
+  function resetFields() {
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
+    document.getElementById("operator").value = "+";
     document.getElementById("result").innerText = "Result: ";
-}
+  }
